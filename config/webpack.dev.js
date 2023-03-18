ef061d40126a98b3dd5890d781009c62fcfc8a81
Vincenzo <vincenzo.delluniversit@virgilio.it>
2023-03-18 17:11:28 +0100

    Setup Dev Server

---
+++ config/webpack.dev.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/main.js",
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/",
+  },
+  target: "web",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "../dist"),
+    },
+    hot: true,
+  },
+};
